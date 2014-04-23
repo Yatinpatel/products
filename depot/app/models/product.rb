af6cae1ff83fda7f21f579f3ee45cb0855c7bd3d
yatin patel <yatinpatel061990@gmail.com>
2014-04-23 12:20:10 -0700

    cart updated

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -5,4 +5,20 @@ validates :price, numericality: { greater_than_or_equal_to: 0.01}
 validates :title, uniqueness: true
 validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png|jpeg)\z}i, message: 'must be a URL for GIF, JPG,PNG or JPEG.'}
 
+has_many :line_items
+
+before_destroy :ensure_not_referenced_by_any_line_item
+
+private
+# ensure that there are no line items referencing this product
+
+def ensure_not_referenced_by_any_line_item
+if line_items.empty?
+return true
+else
+errors.add(:base, 'line Item present')
+return false
+end
+end
+
 end
--- depot/test/models/product_test.rb~
+++ depot/test/models/product_test.rb~
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
- fixtures :products
-  # test "the truth" do
-  #   assert true
-  # end
-
+ 
+    fixtures :products
+ test "product is not valid without a unique title - i18n" do
+ product = product.new(title: 	product(:ruby).title,
+	
+	description: "yyy",
+	price: 1,
+	image_url: "fred.gif")
+assert !product.save
+assert_equal "has already been taken", product.errors[:title].join('; ')
 end
